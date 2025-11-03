a234452d8f7bde81d5ac7773ac03a01395c0a2b9
sidhartthhhhh <singhnishant294@gmail.com>
2025-11-03 10:50:50 +0530

    Longest_palindromic_substring.py

---
+++ longest-palindromic-substring.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def expand_from_center(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left+1:right]  
+        longest = ""
+        for i in range(len(s)):
+            p1 = expand_from_center(i, i)
+            p2 = expand_from_center(i, i + 1)
+            if len(p1) > len(longest):
+                longest = p1
+            if len(p2) > len(longest):
+                longest = p2
+        return longest
