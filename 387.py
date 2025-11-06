655d8f6a7573193adae523c27368bccc4d904b44
sidhartthhhhh <singhnishant294@gmail.com>
2025-11-06 08:59:57 +0530

    387

---
+++ 387.py
@@ -0,0 +1,9 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        counts = collections.Counter(s)
+
+        for i, char in enumerate(s):
+            if counts[char] == 1:
+                return i
+                
+        return -1
